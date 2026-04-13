24a427127f4578c066d9a9d9cbb5a452aaec3681
Sistema_ <correo.tramites.placas@gmail.com>
2026-04-13 11:53:04 -0600

    Delete routes directory

--- routes/engine.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const engineController = require('../controllers/decisionEngine');
-
-router.post('/v1/clients/:id/check-in', engineController.processCheckIn);
-router.get('/v1/trainers/dashboard', engineController.getDashboardData);
-router.get('/v1/clients/:id/plan', engineController.getClientPlan);
-
-module.exports = router;
